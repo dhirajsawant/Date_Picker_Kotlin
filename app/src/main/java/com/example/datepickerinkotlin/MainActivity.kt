f96f4eafaa7952371fdd1a80a21c125ed8b7be4f
dhirajsawant98@gmail.com <Dhiraj@1998>
2021-06-12 18:14:20 +0530

    Date Picker

---
+++ app/src/main/java/com/example/datepickerinkotlin/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.datepickerinkotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.ViewGroup
+import android.widget.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val datePicker = DatePicker(this)
+        val layoutParams = LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT)
+        datePicker.layoutParams = layoutParams
+
+
+        val linearLayout = findViewById<LinearLayout>(R.id.linear_layout)
+        linearLayout?.addView(datePicker)
+
+        val today = Calendar.getInstance()
+        datePicker.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH),
+            today.get(Calendar.DAY_OF_MONTH)
+
+        ) { view, year, month, day ->
+            val month = month + 1
+            val msg = "You Selected: $day/$month/$year"
+            Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+
+        }
+    }
+}
